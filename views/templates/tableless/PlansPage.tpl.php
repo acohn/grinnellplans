42cb31c2286f0ae49ecc4d5095595db0965907b7
Mark Root-Wiley <info@mrwweb.com>
2024-10-27 15:35:31 -0700

    add icon and manifest meta to index.php and the main plans page template. use new SVG logo on homepage

--- views/templates/tableless/PlansPage.tpl.php
+++ views/templates/tableless/PlansPage.tpl.php
@@ -5,6 +5,12 @@
 <head>
 <title><?php echo $this->page_title ?></title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="icon" type="image/png" href="/images/app-icons/favicon-96x96.png" sizes="96x96" />
+<link rel="icon" type="image/svg+xml" href="/images/app-icons/favicon.svg" />
+<link rel="shortcut icon" href="/favicon.ico" />
+<link rel="apple-touch-icon" sizes="180x180" href="/images/app-icons/apple-touch-icon.png" />
+<meta name="apple-mobile-web-app-title" content="Plans" />
+<link rel="manifest" href="/site.webmanifest" />
 <?php foreach($this->stylesheets as $css): ?>
 	<link rel="stylesheet" type="text/css" href="<?php echo $css; ?>">
 <?php
